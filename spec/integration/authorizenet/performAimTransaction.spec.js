7575b0f12340e6d4e96623126a7b290ea687a88e
James Carr <james.r.carr@gmail.com>
2010-08-04 17:37:17 -0400

    added failure behavior for authorize.net AIM

--- spec/integration/authorizenet/performAimTransaction.spec.js
+++ spec/integration/authorizenet/performAimTransaction.spec.js
@@ -4,7 +4,7 @@ require.paths.unshift(__dirname+'/../../../lib/')
 var vows = require('vows'),
     assert = require('assert'),
     clients = require('clients'),
-    authorizenet = require('authorizenet/client'),
+    authorizenet = require('paynode').authorizeNetGateway,
     levels = require('authorizenet/levels').levels,
     sys = require('sys')
 
@@ -14,11 +14,13 @@ var client = authorizenet.createClient({
   ,tran_key:'2jmpG96xM59QAJ4d'
 })
 
+var invalidAuthClient = authorizenet.createClient({
+  level:levels.sandbox
+  ,login:'Bad'
+  ,tran_key:'Login'
+})
 
-vows.describe('AIM Transaction').addBatch({
-   'minimum successful transaction':{
-    topic:function(){
-      client.performAimTransaction({
+var goodTransaction = {
       "x_type": "AUTH_CAPTURE",
       "x_method": "CC",
       "x_card_num": "4111111111111111",
@@ -31,8 +33,14 @@ vows.describe('AIM Transaction').addBatch({
       "x_last_name": "Doe",
       "x_address": "1234 Street",
       "x_state": "WA",
-      "x_zip": "98004"
-      }).on('success', this.callback)
+      "x_zip": "98004"}
+
+
+vows.describe('AIM Transaction').addBatch({
+  'minimum successful transaction':{
+    topic:function(){
+      client.performAimTransaction(goodTransaction)
+        .on('success', this.callback)
     },
     'should complete successfull':function(err, result){
       assert.equal(result.responsecode, 1)
@@ -40,5 +48,36 @@ vows.describe('AIM Transaction').addBatch({
     'should have no error':function(err, result){
       assert.isNull(err)
     }
-   }
+  },
+  'invalid authentication information':{
+    topic:function(){
+      invalidAuthClient.performAimTransaction(goodTransaction)
+        .on('failure', this.callback)
+    },
+    'test':function(err, result){
+      assert.equal(result.responsecode, 3)
+    }
+  },
+  'a failed transaction due to invalid card':{
+    topic:function(){
+      client.performAimTransaction({
+      "x_type": "AUTH_CAPTURE",
+      "x_method": "CC",
+      "x_card_num": "4111111111111111",
+      "x_exp_date": "0105",
+      
+      "x_amount": "19.99",
+      "x_description": "Sample Transaction",
+
+      "x_first_name": "John",
+      "x_last_name": "Doe",
+      "x_address": "1234 Street",
+      "x_state": "WA",
+      "x_zip": "98004"
+      }).on('failure', this.callback)
+     },
+    'has error message':function(err, result){
+      assert.equal(result.responsereasontext, '(TESTMODE) The credit card has expired.')
+    }
+  }
 }).export(module)
